5b9d3ace02d5f5a1c51d4359714adb2382d8c130
ziatdinovamir <ziatdinov.amir.07@gmail.com>
2026-03-03 11:38:14 +0300

    Code without 'using namepace std'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-    string name;
-    cout << "Enter your name: ";
-    getline(cin, name);
-    cout << "Hello world from " << name << endl;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+    std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
