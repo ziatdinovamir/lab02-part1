0150834c42afb53659fd20b3de1838521d8f50f9
ziatdinovamir <ziatdinov.amir.07@gmail.com>
2026-03-03 13:13:46 +0300

    Update comments with  :)))))

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-
 int main() {
-    std::string name;//Users's name.
-    std::cout << "Enter your name: ";//User enters his name
-    std::getline(std::cin, name);//Reading the string with spaces
-    std::cout << "Hello world from " << name << std::endl;//Display the inscription with  User's name
+    std::string name;//Users's name.:)))))
+    std::cout << "Enter your name: ";//User enters his name:)))))
+    std::getline(std::cin, name);//Reading the string with spaces:)))))
+    std::cout << "Hello world from " << name << std::endl;//Display the inscription with  User's name :)))))
     return 0;
 }
